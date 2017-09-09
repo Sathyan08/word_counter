25ef8088e905424fcdcb95010b6d36868d4d6246
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-09-09 13:21:04 -0400

    Added test

--- spec/services/word_counter_spec.rb
+++ spec/services/word_counter_spec.rb
@@ -1,1 +1,13 @@
 require 'spec_helper'
+require './lib/services/word_counter'
+
+RSpec.describe WordCounter do
+  let(:test_string) { "one two Two dos Dos three three Three" }
+  let(:expected_result) { {"three" => 3, "dos" => 2, "two" => 2, "one" => 1} }
+
+  describe "#word_count" do
+    it "returns a hash ordered by word frequency then alphabetically" do
+      expect(subject.execute(test_string)).to eq(expected_result)
+    end
+  end
+end
