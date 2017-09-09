b5f677a17c18fe3462ffb3fa2037abf697a83e6a
Sathyan Mathai <sathyanmathai@Sathyans-MacBook-Pro.local>
2017-09-09 13:37:54 -0400

    Got the tests to pass

--- lib/services/word_counter.rb
+++ lib/services/word_counter.rb
@@ -1,3 +1,29 @@
-class WordCounter
+class WordCounter < Struct.new(:string)
 
+  def self.execute(string)
+    new(string).count_words
+  end
+
+  def count_words
+    words.each do |word|
+      formatted_word = formatted_word(word)
+      word_count[formatted_word] += 1
+    end
+
+    word_count.sort_by { |word, frequency| [-frequency, word] }.to_h
+  end
+
+  private
+
+  def formatted_word(word)
+    word.downcase.strip.gsub(/\W/, '')
+  end
+
+  def words
+    @words ||= string.split(' ')
+  end
+
+  def word_count
+    @word_count ||= Hash.new(0)
+  end
 end
--- spec/services/word_counter_spec.rb
+++ spec/services/word_counter_spec.rb
@@ -4,10 +4,23 @@ require './lib/services/word_counter'
 RSpec.describe WordCounter do
   let(:test_string) { "one two Two dos Dos three three Three" }
   let(:expected_result) { {"three" => 3, "dos" => 2, "two" => 2, "one" => 1} }
+  let(:result) { described_class.execute(test_string) }
 
   describe "#word_count" do
-    it "returns a hash ordered by word frequency then alphabetically" do
-      expect(subject.execute(test_string)).to eq(expected_result)
+    context "when given a string without non-word characters" do
+      it "returns a hash ordered by word frequency then alphabetically" do
+        expect(result).to eq(expected_result)
+        expect(result.to_a).to eq(expected_result.to_a)
+      end
+    end
+
+    context "when given a string containing punctuation or other non-word characters" do
+      let(:test_string) { "one, two. Two dos Dos three three Three!" }
+
+      it "removes the non-word characters and returns a hash ordered by word frequency then alphabetically" do
+        expect(result).to eq(expected_result)
+        expect(result.to_a).to eq(expected_result.to_a)
+      end
     end
   end
 end
